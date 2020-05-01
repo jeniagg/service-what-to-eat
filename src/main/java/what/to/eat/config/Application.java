7a995664b9fcd0681255afd89fd928d33b40b442
Jenia Georgieva <jenia.95@abv.bg>
2020-05-01 13:25:37 +0300

    Fix component scan in order for application to work probably

--- src/main/java/what/to/eat/config/Application.java
+++ src/main/java/what/to/eat/config/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"main.java.what.to.eat"})
+@ComponentScan({"what.to.eat"})
 public class Application {
 
 	public static void main(String[] args) {
