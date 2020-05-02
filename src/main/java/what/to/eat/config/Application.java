6134934884f198eaa9b3eb6cbfd223cedbade978
Jenia Georgieva <jenia.95@abv.bg>
2020-05-02 15:23:33 +0300

    Add initial swagger configuration

--- src/main/java/what/to/eat/config/Application.java
+++ src/main/java/what/to/eat/config/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"what.to.eat.controllers", "what.to.eat.services"})
+@ComponentScan({"what.to.eat.controllers", "what.to.eat.services","what.to.eat.config"})
 @EntityScan({"what.to.eat.entities"})
 @EnableJpaRepositories({"what.to.eat.repositories"})
 public class Application {
--- src/main/java/what/to/eat/controllers/HelloController.java
+++ src/main/java/what/to/eat/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package what.to.eat.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/test", produces = "application/json")
+	@ApiOperation(value = "Retrieve Hello greeting ")
+	@ApiResponse(code = 200, message = "Suceess|OK")
+	@GetMapping(value = "/test")
 	public ResponseEntity<String> printHello() {
 		return new ResponseEntity<>("Hello to what to eat!", HttpStatus.OK);
 	}
