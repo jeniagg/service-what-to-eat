ecfac66d28adb8cc6077e4b2047f3a00cbfabb3c
Jenia Georgieva <jenia.95@abv.bg>
2020-05-02 17:49:00 +0300

    Upgrade spring-boot version. Move from springforx-swagger to openApi

---
+++ src/main/java/what/to/eat/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package what.to.eat.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(new Info().title("What to eat API").description(
+                "You don't know what to eat? We are here to help you decide!"));
+    }
+}
--- src/main/java/what/to/eat/controllers/HelloController.java
+++ src/main/java/what/to/eat/controllers/HelloController.java
@@ -1,18 +1,28 @@
 package what.to.eat.controllers;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "Hello Controller", description = "test controller")
 public class HelloController {
 
-	@ApiOperation(value = "Retrieve Hello greeting ")
-	@ApiResponse(code = 200, message = "Suceess|OK")
-	@GetMapping(value = "/test")
+	@Operation(summary = "Retrieve Hello!", description = "Greeting from the application!", tags = { "test" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "successful operation",
+					content = @Content(schema = @Schema(implementation = ResponseEntity.class))) })
+	@GetMapping(value = "/test", produces = "application/txt")
 	public ResponseEntity<String> printHello() {
 		return new ResponseEntity<>("Hello to what to eat!", HttpStatus.OK);
 	}
