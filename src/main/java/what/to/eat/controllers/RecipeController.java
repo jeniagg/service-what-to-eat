575751dad6c42df180ac8a93ce942928254fab31
Jenia Georgieva <jenia.95@abv.bg>
2020-05-03 19:18:29 +0300

    Add logging to the application

--- src/main/java/what/to/eat/controllers/RecipeController.java
+++ src/main/java/what/to/eat/controllers/RecipeController.java
@@ -8,18 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import what.to.eat.dtos.RecipeDto;
 import what.to.eat.entities.Recipe;
 import what.to.eat.services.RecipeService;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class RecipeController {
 
+    private static final Logger LOGGER = LogManager.getLogger(RecipeController.class);
+
     @Autowired
     private RecipeService recipeService;
 
     @GetMapping(value = "/recipe", produces = "application/json")
     public ResponseEntity<List<RecipeDto>> getAllRecipes() {
+
+        LOGGER.info("Calling getAllRecipes() endpoint .. ");
 
         List<Recipe> recipes = recipeService.getAllRecipeNames();
         ArrayList<RecipeDto> recipeDtos = new ArrayList<>();
